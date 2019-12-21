1350caf2ed036adbf8b9cf8c55ffb1fddc8b369d
rhon-au <31749110+rhon-au@users.noreply.github.com>
2019-12-21 22:48:37 +1100

    Update API access.ps1

--- API access.ps1
+++ API access.ps1
@@ -2,11 +2,14 @@
  $city = "london"
  $urlx = "https://samples.openweathermap.org/data/2.5/weather?q="+$city+"&appid=b6907d289e10d714a6e88b30761fae22"
 #
+#
 # used to invoke API using GET method
  $stuff = Invoke-RestMethod -Uri $urlx -Method Get
 #
+#
 # write raw json
  $stuff
+#
 #
 # write the output of latitude and longitude
  Write-Host ("Longitude: "+$stuff.coord.lon+" , "+"Latitude: "+$stuff.coord.lat)
